ea44bc10015f3b4e6168203dd29b908ce0591463
passi <passi@DESKTOP-C2HL3F1>
2020-04-05 21:20:48 +0900

    1.2 쿼리 메소드 사용하기 복습완료

--- Chapter05/src/test/java/com/rubypaper/QueryMethodTest.java
+++ Chapter05/src/test/java/com/rubypaper/QueryMethodTest.java
@@ -21,8 +21,32 @@ public class QueryMethodTest {
 	
 	@Autowired
 	private BoardRepository boardRepo;
-	
 	/*
+	@Before
+	public void dataPrepare() {
+		for (int i=1; i<200; i++) {
+			Board board = new Board();
+			board.setTitle("테스트 제목 " + i);
+			board.setWriter("테스터");
+			board.setContent("테스트 내용 " + i);
+			board.setCreateDate(new Date());
+			board.setCnt(0L);
+			boardRepo.save(board);
+		}
+	}
+	
+	@Test
+	public void testFindByTitle() {
+		List<Board> boardList = boardRepo.findByTitle("테스트 제목 10");
+		
+		System.out.println(" ## 검색 결과 ## ");
+		for(Board board : boardList)
+			System.out.println(" ==> " + board.toString());
+	}
+	
+	*/
+	
+	
 	@Test
 	public void testFindByTitleContaing() {
 		Pageable paging = PageRequest.of(0, 5, Sort.Direction.DESC,"seq");
@@ -39,7 +63,7 @@ public class QueryMethodTest {
 		for(Board board : boardList)
 			System.out.println(" ==> " + board.toString());
 	}
-	
+	/*
 	@Test
 	public void testFindByTitleContaing() {
 		Pageable paging = PageRequest.of(0, 5, Sort.Direction.DESC,"seq");
@@ -80,28 +104,8 @@ public class QueryMethodTest {
 		for(Board board : boardList)
 			System.out.println(" ==> " + board.toString());
 	}
+	*/
 	
 	
-	@Before
-	public void dataPrepare() {
-		for (int i=1; i<200; i++) {
-			Board board = new Board();
-			board.setTitle("테스트 제목 " + i);
-			board.setWriter("테스터");
-			board.setContent("테스트 내용" + i);
-			board.setCreateDate(new Date());
-			board.setCnt(0L);
-			boardRepo.save(board);
-		}
-	}
-	
-	@Test
-	public void testFindByTitle() {
-		List<Board> boardList = boardRepo.findByTitle("테스트 제목 10");
-		
-		System.out.println(" ## 검색 결과 ## ");
-		for(Board board : boardList)
-			System.out.println(" ==> " + board.toString());
-	}
-	 */
+	 
 }
