80df21513f546163c1a45770e9d24398f0c7d5f8
passi <passi@DESKTOP-C2HL3F1>
2020-04-05 21:34:29 +0900

    1.3 Query 어노테이션 사용 복습 완료

--- Chapter05/src/test/java/com/rubypaper/QueryAnnotationTest.java
+++ Chapter05/src/test/java/com/rubypaper/QueryAnnotationTest.java
@@ -1,17 +1,14 @@
 package com.rubypaper;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.rubypaper.domain.Board;
 import com.rubypaper.persitence.BoardRepository;
 
 @RunWith(SpringRunner.class)
@@ -20,7 +17,24 @@ public class QueryAnnotationTest {
 
 	@Autowired
 	private BoardRepository boardRepo;
+	/*
+	@Test
+	public void testQueryAnnotationTest1() {
+		List<Board> boardList = boardRepo.queryAnnotationTest1("테스트 제목 10");
+		System.out.println(" ### 검색 결과 ###");
+		for (Board board : boardList)
+			System.out.println(" ===> " + board.toString());
+	}
+	*/
+	@Test
+	public void testQueryAnnotationTest1_1() {
+		List<Board> boardList = boardRepo.queryAnnotationTest1("테스트 제목 10");
+		System.out.println(" ### 검색 결과 ###");
+		for (Board board : boardList)
+			System.out.println(" ===> " + board.toString());
+	}
 	
+	/*
 	@Test
 	public void testQueryAnnotationTest4() {
 		Pageable paging = PageRequest.of(0,3,Sort.Direction.DESC,"seq");
@@ -30,7 +44,7 @@ public class QueryAnnotationTest {
 			System.out.println(" ===> " + Arrays.toString(row));
 	}
 	
-	/*
+	
 	@Test
 	public void testQueryAnnotationTest3() {
 		List<Object[]> boardList = boardRepo.queryAnnotationTest3("테스트 제목 10");
@@ -48,23 +62,7 @@ public class QueryAnnotationTest {
 			System.out.println(" ===> " + Arrays.toString(row));
 	}
 	
-	
-	 * 
-	@Test
-	public void testQueryAnnotationTest1_1() {
-		List<Board> boardList = boardRepo.queryAnnotationTest1("테스트 제목 10");
-		System.out.println(" ### 검색 결과 ###");
-		for (Board board : boardList)
-			System.out.println(" ===> " + board.toString());
-	}
-	
-	@Test
-	public void testQueryAnnotationTest1() {
-		List<Board> boardList = boardRepo.queryAnnotationTest1("테스트 제목 10");
-		System.out.println(" ### 검색 결과 ###");
-		for (Board board : boardList)
-			System.out.println(" ===> " + board.toString());
-	}
 	*/
+	
 	
 }
