7192eff7546d05d74d6b925325b0637034024f8b
passi <passi@DESKTOP-C2HL3F1>
2020-04-05 20:47:29 +0900

    1.1 스프링 데이터 JPA 사용하기 (p240~p256) 완료

--- Chapter05/src/test/java/com/rubypaper/BoardRepositoryTest.java
+++ Chapter05/src/test/java/com/rubypaper/BoardRepositoryTest.java
@@ -1,11 +1,14 @@
 package com.rubypaper;
 
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.rubypaper.domain.Board;
 import com.rubypaper.persitence.BoardRepository;
 
 @RunWith(SpringRunner.class)
@@ -13,41 +16,52 @@ import com.rubypaper.persitence.BoardRepository;
 public class BoardRepositoryTest {
 	@Autowired
 	private BoardRepository boardRepo;
+	
+	/*
+	// 등록 테스트 
+	@Test
+	public void testInsertBoard() {
+		Board board = new Board();
+		board.setTitle("첫 번째 게시글");
+		board.setWriter("테스터");
+		board.setContent("잘 등록되나요?");
+		board.setCreateDate(new Date());
+		board.setCnt(0L);
+		boardRepo.save(board);
+	}
+	
+	
+	// 조회 테스트
+	@Test
+	public void testGetBoard() {
+		Board board = boardRepo.findById(1L).get();
+		System.out.println(board.toString());
+	} 
+	
+	
+	
+	// 수정 테스트
+	@Test	
+	public void testUpdateBoard() {
+		System.out.println("=== 1번 게시글 조회 ===");
+		Board board = boardRepo.findById(1L).get();
+		
+		System.out.println("=== 1번 게시글 제목 수정 ===");
+		board.setTitle("제목을 수정했습니다.");
+		boardRepo.save(board);
+	} 
+	 */
 
 	@Test
-	public void testDeleteBoard() {
+	public void  testDeleteBoard() {
 		boardRepo.deleteById(1L);
 	}
 
 	
-//	@Test	
-//	public void testUpdateBoard() {
-//		System.out.println("=== 1번 게시글 조회 ===");
-//		Board board = boardRepo.findById(1L).get();
-//		
-//		System.out.println("=== 1번 게시글 제목 수정 ===");
-//		board.setTitle("제목을 수정했습니다.");
-//		boardRepo.save(board);
-//	} 
+
 
 	
-//	@Test
-//	public void testGetBoard() {
-//		Board board = boardRepo.findById(1L).get();
-//		System.out.println(board.toString());
-//	} 
+
 	
-	
-//	@Test
-//	public void testInsertBoard() {
-//		Board board = new Board();
-//		board.setTitle("첫 번째 게시글");
-//		board.setWriter("테스터");
-//		board.setContent("잘 등록되나요?");
-//		board.setCreateDate(new Date());
-//		board.setCnt(0L);
-//		
-//		boardRepo.save(board);
-//	}
 
 }
